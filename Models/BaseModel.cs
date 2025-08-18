716b1b0b1c763066ac008c0c3474c4203fcc20d9
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-18 15:20:53 +0300

    feat(db): add BaseModel

---
+++ Models/BaseModel.cs
@@ -0,0 +1,8 @@
+namespace SubsTracker.Models;
+
+public abstract class BaseModel
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+}
