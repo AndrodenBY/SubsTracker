c68bb0e48827ce27e2d247637de386f2e2ae4689
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-19 13:32:39 +0300

    feat(db): add subscription model and related

---
+++ SubsTracker.DAL/Models/Subscription/Subscription.cs
@@ -0,0 +1,12 @@
+using SubsTracker.Domain.Enums;
+
+namespace SubsTracker.DAL.Models.Subscription;
+
+public class Subscription: BaseModel
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public DateOnly DueDate {get; set;}
+    public SubscriptionType Type { get; set; }
+    public SubscriptionContent Content { get; set; }
+}
