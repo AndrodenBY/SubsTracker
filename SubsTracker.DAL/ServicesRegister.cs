49fa9dad940d27bebd1f7f9c93d44cfee95bbc26
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-19 13:47:17 +0300

    feat(db): configure dbcontext

---
+++ SubsTracker.DAL/ServicesRegister.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SubsTracker.DAL;
+
+public static class ServicesRegister
+{
+        public static IServiceCollection RegisterContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var postgreConnectionString = configuration.GetConnectionString("PostgreConnectionString");
+            services.AddDbContext<SubsDbContext>(options =>
+                options.UseNpgsql(postgreConnectionString));
+
+            return services;
+        }
+}
---
+++ SubsTracker.DAL/SubsDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SubsTracker.DAL;
+
+public class SubsDbContext : DbContext
+{
+    public DbSet<User> Users {get; set;}
+    public DbSet<Subscription> Subscriptions {get; set;}
+    public DbSet<UserGroup> UserGroups {get; set;}
+    public DbSet<GroupMember> Members {get; set;}
+
+    public SubsDbContext(DbContextOptions<SubsDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
