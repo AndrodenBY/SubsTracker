0f8c96d8e681b3c124baa7129c20fa534ba83479
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-19 15:51:15 +0300

    chore(db): fix propery naming

--- SubsTracker.DAL/Models/User/User.cs
+++ SubsTracker.DAL/Models/User/User.cs
@@ -5,7 +5,7 @@ public class User: BaseModel
     public string FirstName { get; set; } = string.Empty;
     public string? LastName { get; set; }
     public string? Email { get; set; }
-    public int SubsCount { get; set; }
+    public int SubscriptionCount { get; set; }
     public List<Subscription.Subscription>? Subs { get; set; } = new();
     public List<UserGroup>? Groups { get; set; } = new();
 }
