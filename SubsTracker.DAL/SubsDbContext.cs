625f0055a18da66597a11c293036cb6a7445f150
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-20 15:25:43 +0300

    feat(db): correct typo and override SaveChanges()

---
+++ SubsTracker.DAL/SubsDbContext.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using SubsTracker.DAL.Models.Subscription;
+using SubsTracker.DAL.Models.User;
+using SubsTracker.Domain;
+
+namespace SubsTracker.DAL;
+
+public class SubsDbContext : DbContext
+{
+    public DbSet<User> Users {get; set;}
+    public DbSet<Subscription> Subscriptions {get; set;}
+    public DbSet<UserGroup> UserGroups {get; set;}
+    public DbSet<GroupMember> Members {get; set;}
+
+    public SubsDbContext(DbContextOptions<SubsDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public override int SaveChanges()
+    {
+        OnBeforeSaving();
+        return base.SaveChanges();
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        OnBeforeSaving();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void OnBeforeSaving()
+    {
+        var entries = ChangeTracker.Entries<IBaseModel>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State is EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+                entry.Entity.ModifiedAt = DateTime.UtcNow;
+                entry.Entity.Id = Guid.NewGuid();
+            }
+            else if (entry.State is EntityState.Modified)
+            {
+                entry.Entity.ModifiedAt = DateTime.UtcNow;
+            }
+        }
+    }
+}
