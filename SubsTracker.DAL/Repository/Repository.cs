c09cbff56720d2e1785cb5a194efc3266e3f0739
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-25 11:09:54 +0300

    fix(api): fix according to reviews
    add CancellationToken = default and other

--- SubsTracker.DAL/Repository/Repository.cs
+++ SubsTracker.DAL/Repository/Repository.cs
@@ -1,55 +1,49 @@
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using SubsTracker.BLL.Exceptions;
 using SubsTracker.Domain;
 
 namespace SubsTracker.DAL.Repository;
 
-public class Repository<TModel> : IRepository<TModel> where TModel : class, IBaseModel
+public class Repository<TModel>(SubsDbContext context): IRepository<TModel> where TModel : class, IBaseModel
 {
-    private readonly SubsDbContext _context;
-    private readonly DbSet<TModel> _dbSet;
-
-    public Repository(SubsDbContext context)
-    {
-        _context = context;
-        _dbSet = context.Set<TModel>();
-    }
+    private readonly DbSet<TModel> _dbSet = context.Set<TModel>();
     
-    public async Task<IEnumerable<TModel>> GetAll(CancellationToken cancellationToken)
+    public async Task<IEnumerable<TModel>> GetAll(CancellationToken cancellationToken = default)
     {
         return await _dbSet.ToListAsync(cancellationToken);
     }
     
-    public async Task<TModel?> GetById(Guid id, CancellationToken cancellationToken)
+    public async Task<TModel?> GetById(Guid id, CancellationToken cancellationToken = default)
     {
-        return await _dbSet.FindAsync(id, cancellationToken);
+        return await _dbSet.FirstAsync(entity => entity.Id == id, cancellationToken);
     }
     
-    public async Task<TModel> Create(TModel entityToCreate, CancellationToken cancellationToken)
+    public async Task<TModel> Create(TModel entityToCreate, CancellationToken cancellationToken = default)
     {
         await _dbSet.AddAsync(entityToCreate, cancellationToken);
-        await _context.SaveChangesAsync(cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
         return entityToCreate;
     }
     
-    public async Task<TModel?> Update(TModel entityToUpdate, CancellationToken cancellationToken)
+    public async Task<TModel?> Update(TModel entityToUpdate, CancellationToken cancellationToken = default)
     {
-        var existingEntity = await _dbSet.FindAsync(entityToUpdate.Id, cancellationToken)
-            ?? throw new NullReferenceException($"Entity {entityToUpdate.Id} not found");
+        var existingEntity = await _dbSet.FirstAsync(entity => entity.Id == entityToUpdate.Id, cancellationToken)
+            ?? throw new NotFoundException($"Entity {entityToUpdate.Id} not found");
         
-        _context.Update(existingEntity);
-        await _context.SaveChangesAsync(cancellationToken);
+        context.Update(existingEntity);
+        await context.SaveChangesAsync(cancellationToken);
         return existingEntity;
     }
     
-    public async Task<bool> Delete(Guid id, CancellationToken cancellationToken)
+    public async Task<bool> Delete(Guid id, CancellationToken cancellationToken = default)
     {
-        var existingEntity = await _dbSet.FindAsync(id, cancellationToken);
+        var existingEntity = await _dbSet.FirstAsync(entity => entity.Id == id, cancellationToken);
         _dbSet.Remove(existingEntity);
-        return await _context.SaveChangesAsync(cancellationToken) > 0;
+        return await context.SaveChangesAsync(cancellationToken) > 0;
     }
     
-    public async Task<TModel?> FindByCondition(Expression<Func<TModel, bool>> predicate, CancellationToken cancellationToken)
+    public async Task<TModel?> FindByCondition(Expression<Func<TModel, bool>> predicate, CancellationToken cancellationToken = default)
     {
         return await _dbSet.FirstOrDefaultAsync(predicate, cancellationToken);
     }
--- SubsTracker.Domain/Interfaces/IService.cs
+++ SubsTracker.Domain/Interfaces/IService.cs
@@ -6,9 +6,9 @@ public interface IService<TEntity, TDto, TCreateDto, TUpdateDto>
     where TCreateDto : class
     where TUpdateDto : class
 {
-    Task<IEnumerable<TDto>?> GetAll(CancellationToken cancellationToken);
-    Task<TDto?> GetById(Guid id, CancellationToken cancellationToken);
-    Task<TDto> Create(TCreateDto entityToCreate, CancellationToken cancellationToken);
-    Task<TDto> Update(TUpdateDto entityToUpdate, CancellationToken cancellationToken);
-    Task<bool> Delete(Guid id, CancellationToken cancellationToken);
+    Task<IEnumerable<TDto>> GetAll(CancellationToken cancellationToken = default);
+    Task<TDto?> GetById(Guid id, CancellationToken cancellationToken = default);
+    Task<TDto> Create(TCreateDto entityToCreate, CancellationToken cancellationToken = default);
+    Task<TDto> Update(TUpdateDto entityToUpdate, CancellationToken cancellationToken = default);
+    Task<bool> Delete(Guid id, CancellationToken cancellationToken = default);
 }
