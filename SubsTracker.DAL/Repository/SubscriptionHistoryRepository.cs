e2675bea5052a5de8d7b345be14be098401e18d6
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-28 15:18:26 +0300

    feat(db): add SubscriptionHistory repository

---
+++ SubsTracker.DAL/Repository/SubscriptionHistoryRepository.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using SubsTracker.DAL.Interfaces;
+using SubsTracker.DAL.Models.Subscription;
+using SubsTracker.Domain.Enums;
+
+namespace SubsTracker.DAL.Repository;
+
+public class SubscriptionHistoryRepository(SubsDbContext context) : Repository<SubscriptionHistory>(context), ISubscriptionHistoryRepository
+{
+    private readonly DbSet<SubscriptionHistory> _dbSet = context.Set<SubscriptionHistory>();
+    
+    public async Task<bool> Create(Guid subscriptionId, SubscriptionAction action, 
+        decimal? pricePaid, CancellationToken cancellationToken)
+    {
+        var createHistoryItem = new SubscriptionHistory
+        {
+            SubscriptionId = subscriptionId,
+            Action = action,
+            PricePaid = pricePaid,
+        };
+        await _dbSet.AddAsync(createHistoryItem, cancellationToken);
+        return await context.SaveChangesAsync(cancellationToken) > 0;
+    }
+    
+    public async Task UpdateType(SubscriptionType originalType, SubscriptionType updatedType, 
+        Guid subscriptionId, decimal? price, CancellationToken cancellationToken)
+    {
+        if (originalType != updatedType) await Create(subscriptionId, SubscriptionAction.TypeChange, price, cancellationToken);
+    }
+}
