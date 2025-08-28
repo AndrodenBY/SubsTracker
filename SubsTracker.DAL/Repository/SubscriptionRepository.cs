892f2d3841a280eeafa3f46503a58bcb3b6b76da
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-28 15:19:03 +0300

    feat(db): add Subscription repository

---
+++ SubsTracker.DAL/Repository/SubscriptionRepository.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using SubsTracker.DAL.Interfaces;
+using SubsTracker.DAL.Models.Subscription;
+
+
+namespace SubsTracker.DAL.Repository;
+
+public class SubscriptionRepository(SubsDbContext context, SubscriptionHistoryRepository history) : Repository<Subscription>(context), ISubscriptionRepository
+{
+    private readonly DbSet<Subscription> _dbSet = context.Set<Subscription>();
+    
+    public async Task<IEnumerable<Subscription>> GetUpcomingBills(Guid userId, CancellationToken cancellationToken)
+    {
+        var today = DateOnly.FromDateTime(DateTime.Now);
+        var sevenDaysFromNow = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(7));
+    
+        return await _dbSet
+            .Where(s => s.UserId == userId && s.DueDate >= today && s.DueDate <= sevenDaysFromNow)
+            .OrderBy(s => s.DueDate)
+            .ToListAsync(cancellationToken);
+    }
+}
