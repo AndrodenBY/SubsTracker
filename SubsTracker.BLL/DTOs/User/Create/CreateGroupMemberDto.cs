fc2383e23b13f7485251d18fc79bf83bb7a3da23
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-28 09:54:43 +0300

    feat(api): add required dto for member service

---
+++ SubsTracker.BLL/DTOs/User/Create/CreateGroupMemberDto.cs
@@ -0,0 +1,10 @@
+using SubsTracker.Domain.Enums;
+
+namespace SubsTracker.BLL.DTOs.User.Create;
+
+public class CreateGroupMemberDto
+{
+    public Guid UserId { get; set; }
+    public Guid GroupId { get; set; }
+    public MemberRole Role { get; set; }
+}
