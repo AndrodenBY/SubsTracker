5edaec17453af9fbaba31495ecaccb9bb0e3c5f3
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-29 15:02:20 +0300

    feat(api): add user group view models

---
+++ SubsTracker.API/ViewModel/User/Create/CreateUserGroupViewModel.cs
@@ -0,0 +1,6 @@
+namespace SubsTracker.API.ViewModel.User.Create;
+
+public class CreateUserGroupViewModel
+{
+    public string Name { get; set; } = string.Empty;
+}
---
+++ SubsTracker.API/ViewModel/User/Update/UpdateUserGroupViewModel.cs
@@ -0,0 +1,7 @@
+namespace SubsTracker.API.ViewModel.User.Update;
+
+public class UpdateUserGroupViewModel
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+}
