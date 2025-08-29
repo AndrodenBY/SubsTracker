2ac85f5aa61c6a5dd0198f033ed3a2562881da92
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-08-29 15:02:55 +0300

    feat(api): add user view models

---
+++ SubsTracker.API/ViewModel/User/Create/CreateUserViewModel.cs
@@ -0,0 +1,8 @@
+namespace SubsTracker.API.ViewModel.User.Create;
+
+public class CreateUserViewModel
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string? LastName { get; set; }
+    public string Email { get; set; } = string.Empty;
+}
---
+++ SubsTracker.API/ViewModel/User/UserViewModel.cs
@@ -0,0 +1,8 @@
+namespace SubsTracker.API.ViewModel.User;
+
+public class UserViewModel
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+}
