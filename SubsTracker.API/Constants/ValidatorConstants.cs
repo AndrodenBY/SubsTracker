6826a413c52da4e45e79ab46935603c145d907d1
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-01 23:46:32 +0300

    feat(api): add FluentValidation and validation constants

---
+++ SubsTracker.API/Constants/ValidatorConstants.cs
@@ -0,0 +1,8 @@
+namespace SubsTracker.API.Constants;
+
+public static class ValidatorConstants
+{
+    public const int MaximumNameLength = 50;
+    public const int MinimumNameLength = 2;
+    public const decimal MinimumPrice = 0;
+}
