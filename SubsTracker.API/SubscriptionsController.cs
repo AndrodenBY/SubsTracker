aa1eb62bcddfc470be4dc6f45a6c69a425e1ccfd
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-04 15:32:54 +0300

    fix(api): route corrections

--- SubsTracker.API/SubscriptionsController.cs
+++ SubsTracker.API/SubscriptionsController.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using SubsTracker.BLL.DTOs.Subscription;
+using SubsTracker.API.ViewModel.Subscription;
 using SubsTracker.BLL.Interfaces;
 
 namespace SubsTracker.API.Controllers;
@@ -10,15 +11,14 @@ namespace SubsTracker.API.Controllers;
 public class SubscriptionsController(ISubscriptionService service, IMapper mapper) : ControllerBase
 {
     [HttpGet("{id:guid}")]
-    public async Task<SubscriptionViewModel?> GetById(Guid id, CancellationToken cancellationToken)
+    public async Task<SubscriptionViewModel> GetById(Guid id, CancellationToken cancellationToken)
     {
         var getById = await service.GetById(id, cancellationToken);
         return mapper.Map<SubscriptionViewModel>(getById);
     }
 
     [HttpGet("bills/users/{userId:guid}")]
-    public async Task<IEnumerable<SubscriptionViewModel>> GetUpcomingBills(Guid userId,
-        CancellationToken cancellationToken)
+    public async Task<IEnumerable<SubscriptionViewModel>> GetUpcomingBills(Guid userId, CancellationToken cancellationToken)
     {
         var getUpcomingBills = await service.GetUpcomingBills(userId, cancellationToken);
         return mapper.Map<SubscriptionViewModel>(getUpcomingBills);
@@ -31,15 +31,15 @@ public class SubscriptionsController(ISubscriptionService service, IMapper mappe
         return mapper.Map<IEnumerable<SubscriptionViewModel>>(getAll);
     }
     
-    [HttpPost]
-    public async Task<SubscriptionViewModel> Create(Guid userId, CreateSubscriptionDto createDto, CancellationToken cancellationToken)
+    [HttpPost("{userId:guid}")]
+    public async Task<SubscriptionViewModel> Create(Guid userId, [FromBody] CreateSubscriptionDto createDto, CancellationToken cancellationToken)
     {
         var create = await service.Create(userId, createDto, cancellationToken);
         return mapper.Map<SubscriptionViewModel>(create);
     }
     
     [HttpPut("{id:guid}")]
-    public async Task<SubscriptionViewModel> Update(Guid id, UpdateSubscriptionDto updateDto, CancellationToken cancellationToken)
+    public async Task<SubscriptionViewModel> Update(Guid id, [FromBody] UpdateSubscriptionDto updateDto, CancellationToken cancellationToken)
     { 
         var update = await service.Update(id, updateDto, cancellationToken);
         return mapper.Map<SubscriptionViewModel>(update);
