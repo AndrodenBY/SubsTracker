61e491f965c77963b812da9bc56f888c1390950d
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-08 11:55:30 +0300

    feat(api): add User validation

---
+++ SubsTracker.API/Validators/User/CreateUserDtoValidator.cs
@@ -0,0 +1,27 @@
+using FluentValidation;
+using SubsTracker.API.Constants;
+using SubsTracker.API.ViewModel.User.Create;
+using SubsTracker.BLL.DTOs.User.Create;
+
+namespace SubsTracker.API.Validators.User;
+
+public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+{
+    public CreateUserDtoValidator()
+    {
+        RuleFor(model => model.FirstName)
+            .NotEmpty().WithMessage(ValidatorMessages.Required("FirstName"))
+            .Length(ValidatorConstants.MinimumNameLength, ValidatorConstants.MaximumNameLength)
+            .WithMessage(ValidatorMessages.Length("FirstName"));
+        
+        RuleFor(model => model.LastName)
+            .NotEmpty()
+            .Length(ValidatorConstants.MinimumNameLength, ValidatorConstants.MaximumNameLength)
+            .When(model => !string.IsNullOrWhiteSpace(model.LastName))
+            .WithMessage(ValidatorMessages.Length("LastName"));
+        
+        RuleFor(model => model.Email)
+            .NotEmpty().WithMessage(ValidatorMessages.Required("Email"))
+            .EmailAddress().WithMessage(ValidatorMessages.Required("Valid Email"));
+    }
+}
