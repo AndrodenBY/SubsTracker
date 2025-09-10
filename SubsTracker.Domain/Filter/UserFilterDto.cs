2940f7ab07d0fc0842893d3c8553732ed9e7ffaa
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-10 22:05:24 +0200

    feat(api): add filter entities

---
+++ SubsTracker.Domain/Filter/UserFilterDto.cs
@@ -0,0 +1,8 @@
+namespace SubsTracker.BLL.DTOs.Filter;
+
+public class UserFilterDto
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+}
