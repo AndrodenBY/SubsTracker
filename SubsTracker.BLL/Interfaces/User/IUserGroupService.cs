99274de6dade94958ff0082e7efa9efed36291b3
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-11 08:25:57 +0200

    feat(api): modify interfaces according to filters

--- SubsTracker.BLL/Interfaces/User/IUserGroupService.cs
+++ SubsTracker.BLL/Interfaces/User/IUserGroupService.cs
@@ -1,12 +1,15 @@
+using SubsTracker.BLL.DTOs.Filter;
 using SubsTracker.BLL.DTOs.User;
 using SubsTracker.BLL.DTOs.User.Create;
 using SubsTracker.BLL.DTOs.User.Update;
 using SubsTracker.DAL.Models.User;
 
-namespace SubsTracker.BLL.Interfaces;
+namespace SubsTracker.BLL.Interfaces.user;
 
-public interface IUserGroupService : IService<UserGroup, UserGroupDto, CreateUserGroupDto, UpdateUserGroupDto>
+public interface IUserGroupService : 
+    IService<UserGroup, UserGroupDto, CreateUserGroupDto, UpdateUserGroupDto, UserGroupFilterDto>
 {
+    Task<IEnumerable<UserGroupDto>> GetAll(UserGroupFilterDto? filter, CancellationToken cancellationToken);
     Task<bool> LeaveGroup(Guid groupId, Guid userId, CancellationToken cancellationToken);
     Task<GroupMemberDto> JoinGroup(CreateGroupMemberDto createDto, CancellationToken cancellationToken);
     Task<UserGroupDto> ShareSubscription(Guid groupId, Guid subscriptionId, CancellationToken cancellationToken);
