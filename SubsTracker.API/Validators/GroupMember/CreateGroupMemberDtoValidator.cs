b7532d8dda27b1b0fc9f0284bab538eb7842af4a
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-15 15:08:48 +0200

    feat(api): add GroupMember validators

---
+++ SubsTracker.API/Validators/GroupMember/CreateGroupMemberDtoValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using SubsTracker.API.Constants;
+using SubsTracker.API.ViewModel.User.Create;
+using SubsTracker.BLL.DTOs.User.Create;
+
+namespace SubsTracker.API.Validators.GroupMember;
+
+public class CreateGroupMemberDtoValidator : AbstractValidator<CreateGroupMemberDto>
+{
+    public CreateGroupMemberDtoValidator()
+    {
+        RuleFor(model => model.UserId)
+            .NotEmpty()
+            .WithMessage(ValidatorMessages.Required(nameof(CreateGroupMemberDto.UserId)));
+
+        RuleFor(model => model.GroupId)
+            .NotEmpty()
+            .WithMessage(ValidatorMessages.Required(nameof(CreateGroupMemberDto.GroupId)));
+
+        RuleFor(model => model.Role)
+            .IsInEnum()
+            .WithMessage(ValidatorMessages.MustBeValid(nameof(CreateGroupMemberDto.Role)));
+    }
+}
