f6bf750fb2b482e7b3b60f34ad585580b9710446
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-15 16:57:02 +0200

    feat(db): add migration for new property in subscription

--- SubsTracker.DAL/Migrations/20250915143704_Subscription_Activation_Property.Designer.cs
+++ SubsTracker.DAL/Migrations/20250915143704_Subscription_Activation_Property.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SubsTracker.DAL;
@@ -11,9 +12,11 @@ using SubsTracker.DAL;
 namespace SubsTracker.DAL.Migrations
 {
     [DbContext(typeof(SubsDbContext))]
-    partial class SubsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250915143704_Subscription_Activation_Property")]
+    partial class Subscription_Activation_Property
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
