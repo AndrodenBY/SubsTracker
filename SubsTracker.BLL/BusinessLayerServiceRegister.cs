b46f44ffae3b1d4d6491ac5f22c3e37e86eed812
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-16 10:48:27 +0200

    fix(api): fix some BLL dependencies

--- SubsTracker.BLL/BusinessLayerServiceRegister.cs
+++ SubsTracker.BLL/BusinessLayerServiceRegister.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SubsTracker.BLL.Interfaces;
+using SubsTracker.BLL.Interfaces.Subscription;
+using SubsTracker.BLL.Interfaces.user;
+using SubsTracker.BLL.Interfaces.User;
 using SubsTracker.BLL.Mapper;
 using SubsTracker.BLL.Services;
 using SubsTracker.BLL.Services.Subscription;
@@ -15,7 +18,7 @@ public static class BusinessLayerServiceRegister
     {
         services.RegisterDataLayerDependencies(configuration)
         .AddAutoMapper(cfg => { }, typeof(DtoMappingProfile).Assembly)
-        .AddScoped(typeof(IService<,,,>), typeof(Service<,,,>))
+        .AddScoped(typeof(IService<,,,,>), typeof(Service<,,,,>))
         .AddScoped<ISubscriptionService, SubscriptionService>()
         .AddScoped<IUserService, UserService>()
         .AddScoped<IUserGroupService, UserGroupService>();
