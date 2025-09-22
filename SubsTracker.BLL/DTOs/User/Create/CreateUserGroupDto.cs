98912935800d474357278cbf9d65d2dcdfd0be8d
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-22 12:20:46 +0200

    chore(api): change Dto according to controller logic

--- SubsTracker.BLL/DTOs/User/Create/CreateUserGroupDto.cs
+++ SubsTracker.BLL/DTOs/User/Create/CreateUserGroupDto.cs
@@ -2,6 +2,6 @@ namespace SubsTracker.BLL.DTOs.User.Create;
 
 public class CreateUserGroupDto
 {
-    public required Guid UserId { get; set; }
+    public Guid UserId { get; set; }
     public required string Name { get; set; }
 }
