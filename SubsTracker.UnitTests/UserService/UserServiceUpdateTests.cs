b96d9d10962ec5c60e9b71753cf73c2110918104
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-23 09:35:40 +0200

    feat(test): add Update tests for UserService

---
+++ SubsTracker.UnitTests/UserService/UserServiceUpdateTests.cs
@@ -0,0 +1,38 @@
+namespace SubsTracker.UnitTests.UserService;
+
+public class UserServiceUpdateTests : UserServiceTestsBase
+{
+    [Fact]
+    public async Task Update_WhenCalled_ReturnsUpdatedUser()
+    {
+        //Arrange
+        var userEntity = _fixture.Create<User>();
+        var updateDto = _fixture.Build<UpdateUserDto>()
+            .With(userGroup => userGroup.Id, userEntity.Id)
+            .Create();
+        var userDto = _fixture.Build<UserDto>()
+            .With(userGroup => userGroup.Id, updateDto.Id )
+            .Create();
+
+        _repository.GetById(updateDto.Id, default).Returns(userEntity);
+        _repository.Update(Arg.Any<User>(), default).Returns(userEntity);
+        _mapper.Map(updateDto, userEntity).Returns(userEntity);
+        _mapper.Map<UserDto>(userEntity).Returns(userDto);
+
+        //Act
+        var result = await _service.Update(updateDto.Id, updateDto, default);
+
+        //Assert
+        result.ShouldNotBeNull();
+        result.Id.ShouldBeEquivalentTo(userEntity.Id);
+        await _repository.Received(1).Update(Arg.Any<User>(), default);
+    }
+
+    [Fact]
+    public async Task Update_WhenNull_ThrowsInvalidOperationException()
+    {
+        // Act & Assert
+        await Should.ThrowAsync<InvalidOperationException>(async () => await _service.Update(Guid.Empty, null, default));
+    }
+}
+
