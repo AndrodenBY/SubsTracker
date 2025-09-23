97a2c4813d1ade67991586846f9dd1b0a10dd56e
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-23 09:53:23 +0200

    feat(test): add UnshareSubscription tests for UserGroupService

---
+++ SubsTracker.UnitTests/UserGroupService/UserGroupServiceUnshareSubscriptionTests.cs
@@ -0,0 +1,40 @@
+namespace SubsTracker.UnitTests.UserGroupService;
+
+public class UserGroupUnshareSubscriptionTests : UserGroupServiceTestsBase
+{
+    [Fact]
+    public async Task UnshareSubscription_Should_RemoveSubscription_When_DataIsValid()
+    {
+        //Arrange
+        var subscription = new Subscription { Id = Guid.NewGuid() };
+        var userGroup = _fixture.Build<UserGroup>()
+            .With(group => group.SharedSubscriptions, new List<Subscription> { subscription })
+            .Create();
+        var expectedDto = new UserGroupDto { Id = userGroup.Id, Name = userGroup.Name };
+
+        _repository.GetById(userGroup.Id, default)
+            .Returns(userGroup);
+        _repository.Update(Arg.Any<UserGroup>(), default)
+            .Returns(userGroup);
+
+        _mapper.Map<UserGroupDto>(Arg.Any<UserGroup>()).Returns(expectedDto);
+
+        //Act
+        var result = await _service.UnshareSubscription(userGroup.Id, subscription.Id, CancellationToken.None);
+
+        //Assert
+        result.ShouldNotBeNull();
+        await _repository.Received(1).Update(Arg.Is<UserGroup>(g => !g.SharedSubscriptions.Contains(subscription)), default);
+    }
+    
+    [Fact]
+    public async Task UnshareSubscription_Should_ThrowNotFoundException_When_GroupDoesNotExist()
+    {
+        //Arrange
+        _repository.GetById(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+            .Returns((UserGroup)null);
+
+        //Act & Assert
+        await Should.ThrowAsync<NotFoundException>(async () => await _service.UnshareSubscription(Guid.NewGuid(), Guid.NewGuid(), CancellationToken.None));
+    }
+}
