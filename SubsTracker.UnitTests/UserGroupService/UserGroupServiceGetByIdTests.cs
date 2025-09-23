33dba6848ae5b1f101ada4bd3d9ef2adbc1380b9
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-09-23 09:52:50 +0200

    feat(test): add GetById tests for UserGroupService

---
+++ SubsTracker.UnitTests/UserGroupService/UserGroupServiceGetByIdTests.cs
@@ -0,0 +1,80 @@
+namespace SubsTracker.UnitTests.UserGroupService;
+
+public class UserGroupServiceGetByIdTests : UserGroupServiceTestsBase
+{
+    [Fact]
+    public async Task GetById_ShouldReturnUserGroupDto_WhenUserGroupExists()
+    {
+        //Arrange
+        var userGroupDto = _fixture.Create<UserGroupDto>();
+        var userGroup = _fixture.Build<UserGroup>()
+            .With(x => x.Id, userGroupDto.Id)
+            .With(x => x.Name, userGroupDto.Name)
+            .Create();
+
+        _repository.GetById(userGroupDto.Id, default)
+            .Returns(userGroup);
+
+        _mapper.Map<UserGroupDto>(userGroup)
+            .Returns(userGroupDto);
+
+        //Act
+        var result = await _service.GetById(userGroupDto.Id, default);
+
+        //Assert
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(userGroupDto.Id);
+        result.Name.ShouldBe(userGroupDto.Name);
+    }
+
+
+    [Fact]
+    public async Task GetById_ShouldReturnNull_WhenEmptyGuid()
+    {
+        //Arrange
+        var emptyId = Guid.Empty;
+        
+        //Act
+        var emptyIdResult = await _service.GetById(emptyId, default);
+        
+        //Assert
+        emptyIdResult.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturnNull_WhenUserGroupDoesNotExist()
+    {
+        //Arrange
+        var fakeId = Guid.NewGuid();
+        
+        //Act
+        var fakeIdResult = await _service.GetById(fakeId, default);
+        
+        //Assert
+        fakeIdResult.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task GetById_WhenCalled_CallsRepositoryExactlyOnce()
+    {
+        //Arrange
+        var userGroupDto = _fixture.Create<UserGroupDto>();
+        var userGroup = _fixture.Build<UserGroup>()
+            .With(x => x.Id, userGroupDto.Id)
+            .With(x => x.Name, userGroupDto.Name)
+            .Create();
+        
+        _repository.GetById(userGroupDto.Id, default)
+            .Returns(userGroup);
+
+        _mapper.Map<UserGroupDto>(userGroup)
+            .Returns(userGroupDto);
+        
+        //Act
+        await _service.GetById(userGroup.Id, default);
+        
+        //Assert
+        await _repository.Received(1).GetById(userGroup.Id, default);
+        _mapper.Received(1).Map<UserGroupDto>(userGroup);
+    }
+}
