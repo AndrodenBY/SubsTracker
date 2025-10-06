ec033967f26b1d8fff6c4ffc9711002d533c875f
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-06 09:59:33 +0200

    fix(db): minor changes to UserGroup model

--- SubsTracker.DAL/Models/User/UserGroup.cs
+++ SubsTracker.DAL/Models/User/UserGroup.cs
@@ -5,6 +5,6 @@ public class UserGroup : BaseModel
     public string Name { get; set; } = string.Empty;
     public Guid? UserId { get; set; }
     public User? User { get; set; }
-    public List<GroupMember>? Members { get; set; }
-    public List<Subscription.Subscription>? SharedSubscriptions { get; set; }
+    public List<GroupMember>? Members { get; set; } = new();
+    public List<Subscription.Subscription>? SharedSubscriptions { get; set; } = new();
 }
