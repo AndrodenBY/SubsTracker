a8ebc8cd84c6842708ade4a89a7cc7b1d82224c5
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-12 10:58:06 +0200

    fix(dal): resolve primary constructor conflicts

--- SubsTracker.DAL/Repository/Repository.cs
+++ SubsTracker.DAL/Repository/Repository.cs
@@ -7,46 +7,54 @@ namespace SubsTracker.DAL.Repository;
 
 public class Repository<TEntity>(SubsDbContext context) : IRepository<TEntity> where TEntity : class, IBaseModel
 {
+    protected SubsDbContext Context = context;
+
     private readonly DbSet<TEntity> _dbSet = context.Set<TEntity>();
 
     public async Task<List<TEntity>> GetAll(
         Expression<Func<TEntity, bool>>? predicate, CancellationToken cancellationToken)
     {
-        var query = _dbSet.AsQueryable();
+        var query = _dbSet
+            .AsQueryable()
+            .AsNoTracking();
+
         if (predicate is not null)
         {
             query = query.Where(predicate);
         }
+
         return await query.ToListAsync(cancellationToken);
     }
 
     public virtual async Task<TEntity?> GetById(Guid id, CancellationToken cancellationToken)
     {
-        return await _dbSet.FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
+        var entity = await _dbSet.FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
+        return entity;
     }
 
     public async Task<TEntity> Create(TEntity entityToCreate, CancellationToken cancellationToken)
     {
         await _dbSet.AddAsync(entityToCreate, cancellationToken);
-        await context.SaveChangesAsync(cancellationToken);
+        await Context.SaveChangesAsync(cancellationToken);
         return entityToCreate;
     }
 
     public async Task<TEntity> Update(TEntity entityToUpdate, CancellationToken cancellationToken)
     {
-        var existingEntity = context.Update(entityToUpdate);
-        await context.SaveChangesAsync(cancellationToken);
+        var existingEntity = Context.Update(entityToUpdate);
+        await Context.SaveChangesAsync(cancellationToken);
         return existingEntity.Entity;
     }
 
     public async Task<bool> Delete(TEntity entityToDelete, CancellationToken cancellationToken)
     {
         _dbSet.Remove(entityToDelete);
-        return await context.SaveChangesAsync(cancellationToken) > 0;
+        return await Context.SaveChangesAsync(cancellationToken) > 0;
     }
 
     public async Task<TEntity?> GetByPredicate(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken)
     {
-        return await _dbSet.FirstOrDefaultAsync(predicate, cancellationToken);
+        var entityByPredicate = await _dbSet.FirstOrDefaultAsync(predicate, cancellationToken);
+        return entityByPredicate;
     }
 }
