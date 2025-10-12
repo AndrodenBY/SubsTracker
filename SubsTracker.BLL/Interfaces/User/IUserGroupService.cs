5f250c765f09ff6ec2f94ce2100ecce230e9d373
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-12 10:55:57 +0200

    fix(db): update interfaces according to realisation

--- SubsTracker.BLL/Interfaces/User/IUserGroupService.cs
+++ SubsTracker.BLL/Interfaces/User/IUserGroupService.cs
@@ -9,11 +9,11 @@ namespace SubsTracker.BLL.Interfaces.User;
 public interface IUserGroupService :
     IService<UserGroup, UserGroupDto, CreateUserGroupDto, UpdateUserGroupDto, UserGroupFilterDto>
 {
-    Task<UserGroupDto?> GetById(Guid id, CancellationToken cancellationToken);
+    new Task<UserGroupDto?> GetById(Guid id, CancellationToken cancellationToken);
     Task<List<UserGroupDto>> GetAll(UserGroupFilterDto? filter, CancellationToken cancellationToken);
     Task<UserGroupDto> Create(Guid userId, CreateUserGroupDto createDto, CancellationToken cancellationToken);
-    Task<UserGroupDto> Update(Guid updateId, UpdateUserGroupDto updateDto, CancellationToken cancellationToken);
+    new Task<UserGroupDto> Update(Guid updateId, UpdateUserGroupDto updateDto, CancellationToken cancellationToken);
     Task<UserGroupDto> ShareSubscription(Guid groupId, Guid subscriptionId, CancellationToken cancellationToken);
     Task<UserGroupDto> UnshareSubscription(Guid groupId, Guid subscriptionId, CancellationToken cancellationToken);
-    Task<bool> Delete(Guid id, CancellationToken cancellationToken);
+    new Task<bool> Delete(Guid id, CancellationToken cancellationToken);
 }
