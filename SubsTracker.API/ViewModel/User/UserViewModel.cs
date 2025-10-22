ed5f97ff540110aa13652101b79bee13f3e8ba3b
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-22 16:01:16 +0200

    fix(core): add Id to UserViewModel

--- SubsTracker.API/ViewModel/User/UserViewModel.cs
+++ SubsTracker.API/ViewModel/User/UserViewModel.cs
@@ -2,6 +2,7 @@ namespace SubsTracker.API.ViewModel.User;
 
 public class UserViewModel
 {
+    public required Guid Id { get; set; }
     public required string FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
