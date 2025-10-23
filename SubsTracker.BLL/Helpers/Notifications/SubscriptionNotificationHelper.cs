ba823d3f2bcce353f8644f011a5c0ca907ef48ad
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-23 21:37:36 +0200

    fix sonar warnings

--- SubsTracker.BLL/Helpers/Notifications/SubscriptionNotificationHelper.cs
+++ SubsTracker.BLL/Helpers/Notifications/SubscriptionNotificationHelper.cs
@@ -7,30 +7,20 @@ public static class SubscriptionNotificationHelper
 {
     public static SubscriptionCanceledEvent CreateSubscriptionCanceledEvent(Subscription subscription)
     {
-        if (subscription.User is null)
-        {
-            throw new ArgumentNullException(nameof(subscription.User), "User must not be null");
-        }
-
         return new SubscriptionCanceledEvent(
             subscription.Id,
             subscription.Name,
-            subscription.User.Id,
+            subscription.User!.Id,
             subscription.User.Email
         );
     }
 
     public static SubscriptionRenewedEvent CreateSubscriptionRenewedEvent(Subscription subscription)
     {
-        if (subscription.User is null)
-        {
-            throw new ArgumentNullException(nameof(subscription.User), "User must not be null");
-        }
-
         return new SubscriptionRenewedEvent(
             subscription.Id,
             subscription.Name,
-            subscription.User.Id,
+            subscription.User!.Id,
             subscription.DueDate,
             subscription.User.Email
         );
