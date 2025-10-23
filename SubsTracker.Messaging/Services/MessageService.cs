2954700ea8edf474a863c7de5f67c82e4a2bbb1d
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-23 16:16:45 +0200

    feat(api): add logging to MessageService

--- SubsTracker.Messaging/Services/MessageService.cs
+++ SubsTracker.Messaging/Services/MessageService.cs
@@ -1,28 +1,49 @@
 using MassTransit;
+using Microsoft.Extensions.Logging;
 using SubsTracker.Messaging.Contracts;
 using SubsTracker.Messaging.Interfaces;
 
 namespace SubsTracker.Messaging.Services;
 
-public class MessageService(IPublishEndpoint publishEndpoint) : IMessageService
+public class MessageService(IPublishEndpoint publishEndpoint, ILogger<MessageService> logger) : IMessageService
 {
-    public Task NotifySubscriptionCanceled(SubscriptionCanceledEvent canceledSubscription, CancellationToken cancellationToken) 
+    public Task NotifySubscriptionCanceled(SubscriptionCanceledEvent canceledSubscription, CancellationToken cancellationToken)
     {
-        return publishEndpoint.Publish(canceledSubscription, cancellationToken);
+        logger.LogInformation($"Attempting to publish SubscriptionCanceledEvent for SubscriptionId: {canceledSubscription.Id}");
+        
+        var publishedNotification = publishEndpoint.Publish(canceledSubscription, cancellationToken);
+        logger.LogInformation($"Successfully published SubscriptionCanceledEvent for SubscriptionId: {canceledSubscription.Id}");
+        
+        return publishedNotification;
     }
 
     public Task NotifySubscriptionRenewed(SubscriptionRenewedEvent renewedSubscription, CancellationToken cancellationToken)
     {
-        return publishEndpoint.Publish(renewedSubscription, cancellationToken);
+        logger.LogInformation($"Attempting to publish SubscriptionRenewedEvent for SubscriptionId: {renewedSubscription.Id}");
+        
+        var publishedNotification = publishEndpoint.Publish(renewedSubscription, cancellationToken);
+        logger.LogInformation($"Successfully published SubscriptionRenewedEvent for SubscriptionId: {renewedSubscription.Id}");
+        
+        return publishedNotification; 
     }
-    
+
     public Task NotifyMemberChangedRole(MemberChangedRoleEvent member, CancellationToken cancellationToken)
     {
-        return publishEndpoint.Publish(member, cancellationToken);
+        logger.LogInformation($"Attempting to publish MemberChangedRoleEvent for MemberId: {member.Id}");
+        
+        var publishedNotification = publishEndpoint.Publish(member, cancellationToken);
+        logger.LogInformation($"Successfully published MemberChangedRoleEvent for MemberId: {member.Id}");
+            
+        return publishedNotification;
     }
 
     public Task NotifyMemberLeftGroup(MemberLeftGroupEvent leftMember, CancellationToken cancellationToken)
     {
-        return publishEndpoint.Publish(leftMember, cancellationToken);
+        logger.LogInformation($"Attempting to publish MemberLeftGroupEvent for MemberId: {leftMember.Id}");
+        
+        var publishNotification = publishEndpoint.Publish(leftMember, cancellationToken);
+        logger.LogInformation($"Successfully published MemberLeftGroupEvent for MemberId: {leftMember.Id}");
+            
+        return publishNotification;
     }
 }
