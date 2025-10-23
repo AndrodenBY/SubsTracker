2d8276c535ce575af7304c9b37086df39b53ad73
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-10-23 16:31:03 +0200

    feat(api): implement options pattern

--- SubsTracker.Messaging/MessagingLayerRegister.cs
+++ SubsTracker.Messaging/MessagingLayerRegister.cs
@@ -1,7 +1,9 @@
 using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using SubsTracker.Messaging.Interfaces;
+using SubsTracker.Messaging.Options;
 using SubsTracker.Messaging.Services;
 
 namespace SubsTracker.Messaging;
@@ -10,22 +12,20 @@ public static class MessagingLayerRegister
 {
     public static IServiceCollection RegisterMessagingLayerDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddScoped<IMessageService, MessageService>();
+        services.Configure<RabbitMqOptions>(configuration.GetSection(RabbitMqOptions.SectionName));
         
-        var userName = configuration["RabbitMQ:UserName"];
-        var password = configuration["RabbitMQ:Password"];
-        var hostName = configuration["RabbitMQ:HostName"];
-        var virtualHostName = configuration["RabbitMQ:VirtualHostName"];
+        services.AddSingleton<IMessageService, MessageService>();
         
         services.AddMassTransit(busConfigurator =>
         {
             busConfigurator.SetKebabCaseEndpointNameFormatter();
             busConfigurator.UsingRabbitMq((context, rabbitMqConfigurator) =>
             {
-                rabbitMqConfigurator.Host(hostName, virtualHostName, configure =>
+                var rabbitMqOptions = context.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+                rabbitMqConfigurator.Host(rabbitMqOptions.HostName, rabbitMqOptions.VirtualHostName, configure =>
                 {
-                    configure.Username(userName);
-                    configure.Password(password);
+                    configure.Username(rabbitMqOptions.UserName);
+                    configure.Password(rabbitMqOptions.Password);
                 });
                 
                 rabbitMqConfigurator.ConfigureEndpoints(context);
