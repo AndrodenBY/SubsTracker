4f362d5b4bf0adfaa1e988c71e8c0f4df9bcd3ee
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-02-11 12:16:54 +0100

    return the tests in usergroupscontroller

--- SubsTracker.IntegrationTests/UserGroup/UserGroupsControllerTests.cs
+++ SubsTracker.IntegrationTests/UserGroup/UserGroupsControllerTests.cs
@@ -29,6 +29,114 @@ public class UserGroupsControllerTests :
     public async Task DisposeAsync() => await _harness.Stop();
 
     [Fact]
+    public async Task GetById_WhenValid_ReturnsCorrectGroup()
+    {
+        //Arrange
+        var seedData = await _dataSeedingHelper.AddOnlyUserGroup();
+
+        //Act
+        var response = await _client.GetAsync($"{EndpointConst.Group}/{seedData.Group.Id}");
+
+        //Assert
+        await _assertHelper.GetByIdValidAssert(response, seedData.Group);
+    }
+
+    [Fact]
+    public async Task GetAll_WhenFilteredByName_ReturnsMatchingGroup()
+    {
+        //Arrange
+        var seedData = await _dataSeedingHelper.AddOnlyUserGroup();
+
+        //Act
+        var response = await _client.GetAsync($"{EndpointConst.Group}?name={seedData.Group.Name}");
+
+        //Assert
+        await _assertHelper.GetAllValidAssert(response, seedData.Group.Name);
+    }
+
+    [Fact]
+    public async Task GetAll_WhenFilteredByNonExistentName_ReturnsEmptyList()
+    {
+        //Arrange
+        await _dataSeedingHelper.AddOnlyUserGroup();
+
+        //Act
+        var response = await _client.GetAsync($"{EndpointConst.Group}?name=NonExistent");
+
+        //Assert
+        await _assertHelper.GetAllInvalidAssert(response);
+    }
+
+    [Fact]
+    public async Task Create_WhenValidData_ReturnsCreatedGroup()
+    {
+        //Arrange
+        var createDto = _dataSeedingHelper.AddCreateUserGroupDto();
+        var seedUser = await _dataSeedingHelper.AddSeedUserOnly();
+
+        //Act
+        var response = await _client.PostAsJsonAsync($"{EndpointConst.Group}/{seedUser.User.Id}/create", createDto);
+
+        //Assert
+        await _assertHelper.CreateValidAssert(response, createDto);
+    }
+
+    [Fact]
+    public async Task Update_WhenValidData_ReturnsUpdatedGroup()
+    {
+        //Arrange
+        var seedData = await _dataSeedingHelper.AddOnlyUserGroup();
+        var updateDto = _dataSeedingHelper.AddUpdateUserGroupDto(seedData.Group.Id);
+
+        //Act
+        var response = await _client.PutAsJsonAsync($"{EndpointConst.Group}/{seedData.Group.Id}", updateDto);
+
+        //Assert
+        await _assertHelper.UpdateValidAssert(response, seedData.Group.Id, "Updated Group Name");
+    }
+
+    [Fact]
+    public async Task Delete_WhenValidId_RemovesGroup()
+    {
+        //Arrange
+        var seedData = await _dataSeedingHelper.AddOnlyUserGroup();
+
+        //Act
+        var response = await _client.DeleteAsync($"{EndpointConst.Group}/{seedData.Group.Id}");
+
+        //Assert
+        await _assertHelper.DeleteValidAssert(response, seedData.Group.Id);
+    }
+
+    [Fact]
+    public async Task GetAllMembers_WhenFilteredByRole_ReturnsCorrectMember()
+    {
+        //Arrange
+        var seedData = await _dataSeedingHelper.AddUserGroupWithMembers();
+        var targetRole = MemberRole.Admin;
+
+        //Act
+        var response = await _client.GetAsync($"{EndpointConst.Group}/members?role={targetRole}");
+
+        //Assert
+        await _assertHelper.GetAllMembersValidAssert(response, seedData, targetRole);
+    }
+
+    [Fact]
+    public async Task JoinGroup_WhenValidData_AddsMember()
+    {
+        //Arrange
+        var seedData = await _dataSeedingHelper.AddUserGroupAndUser();
+        var createDto = await _dataSeedingHelper.AddCreateGroupMemberDto(seedData.Group.Id);
+
+        //Act
+        var response = await _client.PostAsJsonAsync($"{EndpointConst.Group}/join", createDto);
+
+        //Assert
+        await _assertHelper.JoinGroupValidAssert(response, createDto);
+    }
+    
+    [Fact]
     public async Task ChangeRole_WhenValid_ShouldPublishMemberChangedRoleEvent()
     {
         var member = await _dataSeedingHelper.AddMemberOnly();
