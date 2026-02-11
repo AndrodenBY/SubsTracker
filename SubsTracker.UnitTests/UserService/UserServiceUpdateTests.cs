2bcb9626413e3264800759a5fd55551a089e16d5
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-02-11 11:32:10 +0100

    fix update unit test

--- SubsTracker.UnitTests/UserService/UserServiceUpdateTests.cs
+++ SubsTracker.UnitTests/UserService/UserServiceUpdateTests.cs
@@ -10,6 +10,9 @@ public class UserServiceUpdateTests : UserServiceTestsBase
         var updateDto = Fixture.Create<UpdateUserDto>();
         var userDto = Fixture.Create<UserDto>();
         
+        UserRepository.GetByAuth0Id(userDto.Auth0Id, default)
+            .Returns(userEntity);
+
         UserRepository.Update(Arg.Any<User>(), default).Returns(userEntity);
         Mapper.Map(updateDto, userEntity).Returns(userEntity);
         Mapper.Map<UserDto>(userEntity).Returns(userDto);
@@ -19,9 +22,9 @@ public class UserServiceUpdateTests : UserServiceTestsBase
 
         //Assert
         result.ShouldNotBeNull();
-        result.Id.ShouldBeEquivalentTo(userEntity.Id);
         await UserRepository.Received(1).Update(Arg.Any<User>(), default);
     }
+
 
     [Fact]
     public async Task Update_WhenNull_NotFoundException()
