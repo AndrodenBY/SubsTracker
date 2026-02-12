cb4d06a24459ce61bb8fe85db3212cddae8744bc
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-02-12 07:24:07 +0100

    chore: clean up unused classes

--- SubsTracker.API/ViewModel/User/Create/CreateUserViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SubsTracker.API.ViewModel.User.Create;
-
-public class CreateUserViewModel
-{
-    public string Auth0Id { get; set; } = string.Empty;
-    public required string FirstName { get; set; }
-    public string? LastName { get; set; }
-    public required string Email { get; set; }
-}
