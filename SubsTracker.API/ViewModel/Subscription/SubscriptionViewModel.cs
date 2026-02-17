a6167f1cbe0119bf1d98ba59b4ed5a33863c2b13
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-02-17 08:02:21 +0100

    feat(api): add subscription status to dto and viewmodel

--- SubsTracker.API/ViewModel/Subscription/SubscriptionViewModel.cs
+++ SubsTracker.API/ViewModel/Subscription/SubscriptionViewModel.cs
@@ -10,4 +10,5 @@ public class SubscriptionViewModel
     public DateOnly DueDate { get; set; }
     public SubscriptionType Type { get; set; }
     public SubscriptionContent Content { get; set; }
+    public bool Active { get; set; }
 }
