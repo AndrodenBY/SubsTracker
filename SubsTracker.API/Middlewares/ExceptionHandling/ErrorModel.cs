1886dca3f814a1ffc2d5a1e1d48ac9f883acc998
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-03-10 13:18:11 +0100

    fix: exception handling

--- SubsTracker.API/Middlewares/ExceptionHandling/ErrorModel.cs
+++ SubsTracker.API/Middlewares/ExceptionHandling/ErrorModel.cs
@@ -2,10 +2,8 @@ using System.Text.Json.Serialization;
 
 namespace SubsTracker.API.Middlewares.ExceptionHandling;
 
-public class ErrorModel(int statusCode, string? message, object? errors = null)
+public class ErrorModel(int statusCode, string? message)
 {
     public int StatusCode { get; set; } = statusCode;
     public string? Message { get; set; } = message;
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public object? Errors { get; set; } = errors;
 }
--- SubsTracker.API/Middlewares/ExceptionHandling/ExceptionHandlingMiddleware.cs
+++ SubsTracker.API/Middlewares/ExceptionHandling/ExceptionHandlingMiddleware.cs
@@ -7,7 +7,6 @@ namespace SubsTracker.API.Middlewares.ExceptionHandling;
 
 public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
 {
-    
     public async Task Invoke(HttpContext httpContext)
     {
         try
@@ -16,41 +15,13 @@ public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<Exception
         }
         catch (Exception exception)
         {
-            if (exception is ValidationException fluentException)
-            {
-                var validationErrors = fluentException.Errors
-                    .GroupBy(e => e.PropertyName)
-                    .ToDictionary(
-                        g => g.Key,
-                        g => g.Select(e => e.ErrorMessage).ToArray()
-                    );
-                
-                logger.LogWarning("Validation failed at {Path} | Errors: {@ValidationErrors}", 
-                    httpContext.Request.Path, 
-                    validationErrors);
+            logger.LogError(exception, "Unhandled Exception at {Method} {Path}",
+                httpContext.Request.Method,
+                httpContext.Request.Path);
 
-                await HandleValidationResponse(httpContext, validationErrors);
-            }
-            else
-            {
-                logger.LogError(exception, "Unhandled Exception at {Method} {Path}",
-                    httpContext.Request.Method,
-                    httpContext.Request.Path);
-
-                await HandleExceptionResponse(httpContext, exception);
-            }
+            await HandleExceptionResponse(httpContext, exception);
         }
     }
-    
-    private static async Task HandleValidationResponse(HttpContext context, IDictionary<string, string[]> errors)
-    {
-        context.Response.ContentType = MediaTypeNames.Application.Json;
-        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-
-        var errorModel = new ErrorModel(context.Response.StatusCode, "One or more validation errors occurred.", errors);
-        await context.Response.WriteAsJsonAsync(errorModel);
-    }
-
     private static async Task HandleExceptionResponse(HttpContext context, Exception exception)
     {
         context.Response.ContentType = MediaTypeNames.Application.Json;
