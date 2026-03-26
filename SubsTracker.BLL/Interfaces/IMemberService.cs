d53153da0d512a26451e3471b146957ec4882528
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-03-26 16:05:13 +0100

    refactor: cleanup MemberService

--- SubsTracker.BLL/Interfaces/IMemberService.cs
+++ SubsTracker.BLL/Interfaces/IMemberService.cs
@@ -7,11 +7,13 @@ using SubsTracker.Domain.Pagination;
 
 namespace SubsTracker.BLL.Interfaces;
 
-public interface IMemberService : IService<MemberEntity, MemberDto, CreateMemberDto, UpdateMemberDto, MemberFilterDto>
+public interface IMemberService
 {
     Task<MemberDto?> GetFullInfoById(Guid id, CancellationToken cancellationToken);
+    Task<MemberDto?> GetById(Guid id, CancellationToken cancellationToken);
     Task<PaginatedList<MemberDto>> GetAll(MemberFilterDto? filter, PaginationParameters? paginationParameters, CancellationToken cancellationToken);
     Task<MemberDto> JoinGroup(CreateMemberDto createDto, CancellationToken cancellationToken);
-    Task<bool> LeaveGroup(Guid groupId, Guid userId, CancellationToken cancellationToken);
     Task<MemberDto> ChangeRole(Guid memberId, CancellationToken cancellationToken);
+    Task<bool> LeaveGroup(Guid groupId, Guid userId, CancellationToken cancellationToken);
+    
 }
--- SubsTracker.BLL/Services/MemberService.cs
+++ SubsTracker.BLL/Services/MemberService.cs
@@ -9,6 +9,7 @@ using SubsTracker.BLL.Interfaces;
 using SubsTracker.BLL.Interfaces.Cache;
 using SubsTracker.BLL.Mediator.Handlers.JoinGroup;
 using SubsTracker.BLL.Mediator.Signals;
+using SubsTracker.BLL.RedisSettings;
 using SubsTracker.DAL.Entities;
 using SubsTracker.DAL.Interfaces.Repositories;
 using SubsTracker.Domain.Enums;
@@ -22,24 +23,67 @@ public class MemberService(
     IMapper mapper,
     ICacheService cacheService,
     IMediator mediator) 
-    : Service<MemberEntity, MemberDto, CreateMemberDto, UpdateMemberDto, MemberFilterDto>(memberRepository, mapper, cacheService),
-      IMemberService
+    : IMemberService
 {
     public async Task<MemberDto?> GetFullInfoById(Guid id, CancellationToken cancellationToken)
     {
         var memberWithEntities = await memberRepository.GetFullInfoById(id, cancellationToken);
-        return Mapper.Map<MemberDto>(memberWithEntities);
+        return mapper.Map<MemberDto>(memberWithEntities);
+    }
+
+    public async Task<MemberDto?> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var cacheKey = RedisKeySetter.SetCacheKey<MemberEntity>(id);
+        var result = await cacheService.CacheDataWithLock(cacheKey, GetEntity, cancellationToken)
+                     ?? throw new UnknownIdentifierException($"Entity with {id} not found");
+        
+        return result;
+        
+        async Task<MemberDto?> GetEntity()
+        {
+            var entity = await memberRepository.GetById(id, cancellationToken);
+            return mapper.Map<MemberDto>(entity);
+        }
     }
 
     public async Task<PaginatedList<MemberDto>> GetAll(MemberFilterDto? filter, PaginationParameters? paginationParameters, CancellationToken cancellationToken)
     {
         var expression = MemberFilterHelper.CreatePredicate(filter);
-        return await base.GetAll(expression, paginationParameters, cancellationToken);
+        var pagedEntities = await memberRepository.GetAll(expression, paginationParameters, cancellationToken);
+        return mapper.Map<PaginatedList<MemberDto>>(pagedEntities);
     }
 
     public async Task<MemberDto> JoinGroup(CreateMemberDto createDto, CancellationToken cancellationToken)
     {
         return await mediator.Send(new JoinGroup(createDto), cancellationToken);
+    }
+    
+    public async Task<MemberDto> ChangeRole(Guid memberId, CancellationToken cancellationToken)
+    {
+        var memberToUpdate = await memberRepository.GetFullInfoById(memberId, cancellationToken)
+                             ?? throw new UnknownIdentifierException($"Member with id {memberId} not found.");
+
+        var newRole = memberToUpdate.Role switch
+        {
+            MemberRole.Participant => MemberRole.Moderator,
+            MemberRole.Moderator => MemberRole.Participant,
+            _ => throw new PolicyViolationException("Cannot modify administrator role")
+        };
+
+        var updateDto = new UpdateMemberDto { Id = memberToUpdate.Id, Role = newRole };
+        mapper.Map(updateDto, memberToUpdate);
+        
+        var updatedMember = await memberRepository.Update(memberToUpdate, cancellationToken);
+
+        await mediator.Publish(new MemberSignals.ChangedRole(
+                updatedMember.Id, 
+                updatedMember.GroupId,
+                updatedMember.UserId,
+                updatedMember.Group.Name, 
+                updatedMember.User.Email, 
+                updatedMember.Role), 
+            cancellationToken);
+        return mapper.Map<MemberDto>(updatedMember);
     }
 
     public async Task<bool> LeaveGroup(Guid groupId, Guid userId, CancellationToken cancellationToken)
@@ -54,33 +98,9 @@ public class MemberService(
                 memberToDelete.Group.Name, 
                 memberToDelete.User.Email), 
             cancellationToken);
+        
         return await memberRepository.Delete(memberToDelete, cancellationToken);
     }
 
-    public async Task<MemberDto> ChangeRole(Guid memberId, CancellationToken cancellationToken)
-    {
-        var memberToUpdate = await memberRepository.GetFullInfoById(memberId, cancellationToken)
-                             ?? throw new UnknownIdentifierException($"Member with id {memberId} not found.");
-
-        var newRole = memberToUpdate.Role switch
-        {
-            MemberRole.Participant => MemberRole.Moderator,
-            MemberRole.Moderator => MemberRole.Participant,
-            _ => throw new PolicyViolationException("Cannot modify administrator role")
-        };
-
-        var updateDto = new UpdateMemberDto { Id = memberToUpdate.Id, Role = newRole };
-        Mapper.Map(updateDto, memberToUpdate);
-        var updatedMember = await memberRepository.Update(memberToUpdate, cancellationToken);
-
-        await mediator.Publish(new MemberSignals.ChangedRole(
-                updatedMember.Id, 
-                updatedMember.GroupId,
-                updatedMember.UserId,
-                updatedMember.Group.Name, 
-                updatedMember.User.Email, 
-                updatedMember.Role), 
-            cancellationToken);
-        return Mapper.Map<MemberDto>(updatedMember);
-    }
+    
 }
